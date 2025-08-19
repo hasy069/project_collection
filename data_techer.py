04e75a4440493d4da7765f7f42e1774c6fbc75c2
hasi <scouts_smarty_0k@icloud.com>
2025-08-19 19:31:49 +0200

    Incl. Futures Option and new lists of coins
    new Version includes grouped Listings and the Feature to get futures data by setting the mode

--- data_techer.py
+++ data_techer.py
@@ -10,10 +10,18 @@ import gc
 #%%
 
 session = requests.Session()  
-# all your session.get(...) calls will reuse the same TCP/TLS socket
 
-def get_binance_minute_data(symbol: str, start_ms: str, end_ms: str, interval_time: str) -> pd.DataFrame:
-    url = "https://api.binance.com/api/v3/klines"
+
+def get_binance_minute_data(symbol: str, start_ms: str, end_ms: str, interval_time: str, mode: str) -> pd.DataFrame:
+    
+    if mode == "spot":
+        url = "https://api.binance.com/api/v3/klines"
+        addon_ = ""
+        
+    elif mode == "future":
+        url = "https://fapi.binance.com/fapi/v1/klines"
+        addon_ = "FUT_"
+        
     interval = interval_time
     frames = []
     
@@ -41,25 +49,30 @@ def get_binance_minute_data(symbol: str, start_ms: str, end_ms: str, interval_ti
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
         df.set_index('timestamp', inplace=True)
         
-        frames.append(df[['close']].astype(float).rename(columns={'close': symbol}))
+        frames.append(df.astype(float))
+        #frames.append(df[['close']].astype(float).rename(columns={'close': addon_+symbol}))    Old Line above, only Close
         
         # advance start to one ms after last candle
         last_ts = int(data[-1][0])
         start_ms = last_ts + 60_000
-        print(f"Requested {symbol} for the time startting {pd.to_datetime(start_ms, unit = "ms")}")
+        print(f"Requested {symbol} for the time startting {pd.to_datetime(start_ms, unit = 'ms')}")
         time.sleep(0.5)  # throttle to avoid API limits
     
-    print(len(frames))
+    print(f"Bulk Downloaded for time-sub-index in {len(frames)} chunks")
     
-    if frames:
-        return pd.concat(frames)
-    else:
-        return pd.DataFrame(columns=[symbol])
+    return pd.concat(frames) if frames else None
 
 #%%
 
-def get_top_20_minute_closes(start_: str, end_: str, tickers: list) -> pd.DataFrame:
-    interval = "5m"
+def get_total_data(start_: str, end_: str, tickers: list, mode = "spot") -> pd.DataFrame:
+    
+    if mode == "future":
+        addon_ = "FUT_"
+    else:
+        addon_ = ""
+        
+    
+    interval = "1m"
     all_dfs = []
     start = int(pd.to_datetime(start_).timestamp() * 1000)   #changed start_ms to start
     end_ms = int(pd.to_datetime(end_).timestamp() * 1000)
@@ -77,52 +90,60 @@ def get_top_20_minute_closes(start_: str, end_: str, tickers: list) -> pd.DataFr
         
         i += 1
         m = len(tickers)
+        print("----------------------------------------------------------------------------------------")
         print(f"Starting {symbol}")
         symbol_list = []
 
         while start < end_ms:
             current_end = min(current_end + 500 * 60 * 60 * 1000, end_ms)
-            print(f"Fetching {symbol}, [{i} / {m}] from {pd.to_datetime(start, unit = "ms")} to {pd.to_datetime(current_end, unit="ms")}")
-            df_symbol = get_binance_minute_data(symbol, start, current_end, str(interval))
-            symbol_list.append(df_symbol)
+            print(f"Fetching {symbol}, [{i} / {m}] from {pd.to_datetime(start, unit = 'ms')} to {pd.to_datetime(current_end, unit='ms')}")
+            df_symbol = get_binance_minute_data(symbol, start, current_end, str(interval), mode)
+            
+            if df_symbol is not None:
+                symbol_list.append(df_symbol)
+        
+
             start = current_end 
             
             if current_end >= end_ms:
                 break
         
-#hier fehlt der Block 
         non_empty = [a for a in symbol_list if not a.empty ]
-        symbol_total = pd.concat(non_empty, axis = 0)
-        symbol_total = symbol_total[~symbol_total.index.duplicated(keep='first')]  # FIX HIER
-        symbol_total.sort_index(inplace = True)
-        symbol_total.to_csv(f"{symbol}.csv")
-        all_dfs.append(symbol_total)
+        print("Downloaded all data over time for: ", symbol)
+        
+        if non_empty:
+            symbol_total = pd.concat(non_empty, axis = 0)
+            symbol_total.index = pd.to_datetime(symbol_total.index)
+            symbol_total = symbol_total[~symbol_total.index.duplicated(keep='first')]  # FIX HIER
+            symbol_total.sort_index(inplace = True)
+            symbol_total.to_csv(f"{symbol}.csv")
+            symbol_closes = symbol_total["close"].rename({'close': addon_+symbol})
+            
+            all_dfs.append(symbol_closes)
+        else:
+            1 == 1
         print(f"Done for {symbol}")
+        print("----------------------------------------------------------------------------------------")
+
+
+    print("----------------------------------------------------------------------------------------")
+    print("Done for all Symbls, dataframes will be merged and saved into one csv :) Have a happy Day")
+    print("----------------------------------------------------------------------------------------")
 
         
     # merge on index (timestamp), outer join to keep all times
     if all_dfs:
         merged = pd.concat(all_dfs, axis=1)
-        # optionally sort the index
         merged.sort_index(inplace=True)
         return merged
     else:
         return pd.DataFrame()
 
-#%%
 
-            non_empty = [a for a in symbol_list if not a.empty ]
-            symbol_total = pd.concat(non_empty, axis = 0)
-            symbol_total = symbol_total[~symbol_total.index.duplicated(keep='first')]  # FIX HIER
-            symbol_total.sort_index(inplace = True)
-            symbol_total.to_csv(f"{symbol}.csv")
-            all_dfs.append(symbol_total)
-            print(all_dfs)
-            print(f"Done for {symbol}")
 #%%
 
 top_symbols = [
-        "BNBUSDT"
+        "ETHUSDT"
     ]
 
 top_symbols2 = [
@@ -134,17 +155,82 @@ top_symbols2 = [
 ]
 
 top_symbols3 = [
-    "ETHUSDT", "BNBUSDT", "XRPUSDT", "ADAUSDT"
+    "LTCUSDC"
 ]
+
+top_100_pairs = [
+    "BTCUSDT", "ETHUSDT", "XRPUSDT", "USDCUSDT", "BUSDUSDT",
+    "ADAUSDT", "SOLUSDT", "DOGEUSDT", "MATICUSDT", "DOTUSDT",
+    "TRXUSDT", "LINKUSDT", "UNIUSDT", "WBTCUSDT", "AVAXUSDT",
+    "SHIBUSDT", "MANAUSDT", "SANDUSDT", "ATOMUSDT", "LUNA2USDT",
+    "LTCUSDT", "NEARUSDT", "ICPUSDT", "FILUSDT", "FTTUSDT",
+    "AAVEUSDT", "ALGOUSDT", "XLMUSDT", "AXSUSDT", "EOSUSDT",
+    "THETAUSDT", "XTZUSDT", "VETUSDT", "HBARUSDT", "CHZUSDT",
+    "FTMUSDT", "EGLDUSDT", "SUSHIUSDT", "CRVUSDT", "MKRUSDT",
+    "SNXUSDT", "COMPUSDT", "KSMUSDT", "ZECUSDT", "BTTCUSDT",
+    "MINAUSDT", "ENJUSDT", "GRTUSDT", "QNTUSDT", "KNCUSDT",
+    "CELRUSDT", "RUNEUSDT", "ZILUSDT", "CAKEUSDT", "NEOUSDT",
+    "DASHUSDT", "NEXOUSDT", "IOTAUSDT", "FLOWUSDT", "GALAUSDT",
+    "AMPUSDT", "RVNUSDT", "CHSBUSDT", "MELEOUSDT", "ANKRUSDT",
+    "EOSUSDT", "ARUSDT", "ONTUSDT", "TWTUSDT", "QTUMUSDT",
+    "XEMUSDT", "BATUSDT", "HOTUSDT", "LSKUSDT", "SCRTUSDT",
+    "OMGUSDT", "BCHUSDT", "STXUSDT", "ZRXUSDT", "1INCHUSDT",
+    "YFIUSDT", "DCRUSDT", "XVGUSDT", "BTGUSDT", "NMRUSDT",
+    "ICXUSDT", "KAVAUSDT", "ZENUSDT", "WAVESUSDT", "PAXGUSDT",
+    "XVGUSDT", "ADXUSDT", "UMAUSDT", "BALUSDT", "ARPAUSDT",
+    "RENUSDT", "NANOUSDT", "BTSUSDT", "SKLUSDT", "ONTUSDT"
+]
+
 
 
 short_data = top_symbols3
 
-df_merged = get_top_20_minute_closes("2025-07-01 00:00:00", "2025-07-10 00:00:00", short_data)
-df_merged.to_csv("raw_data.csv", index = True)
+future = get_total_data("2025-07-01 00:00:00", "2025-07-10 00:00:00", top_symbols, "future")
+future.to_csv("future_data.csv", index = True)
+
+#spot = get_total_data("2025-06-01 00:00:00", "2025-07-10 00:00:00", short_data, "spot")
+#spot.to_csv("spot_data.csv", index = True)
 
 #df_merged = pd.read_csv("raw_data.csv")
 #df_merged.head(3)
 
 #%%
+
+import matplotlib.pyplot as plt
+
+spot_v = spot.values
+future_v = future.values
+
+plt.plot(future_v-spot_v)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
